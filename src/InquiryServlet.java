011f1bf015b9d29166d2994774cb74677404fd22
xtkcx <1117take@gmail.com>
2024-06-27 02:36:46 +0900

    サーブレットの作成

--- src/InquiryServlet.java
+++ src/InquiryServlet.java
@@ -1,41 +1,24 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class InquiryServlet
- */
 @WebServlet("/InquiryServlet")
 public class InquiryServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InquiryServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public InquiryServlet() {
+		super();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		String name = request.getParameter("name");
+		System.out.println(name);
+		PrintWriter out=response.getWriter();
+		out.println("<html><head></head><body><br>"+name+"さん、お問合せありがとうございました</body></html>");
 	}
-
 }
